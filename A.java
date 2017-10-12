6a7e12f4c935853ab21cc3f2e79e5b9c7a7751d3
zadesrujan <31057499+zadesrujan@users.noreply.github.com>
2017-10-12 10:19:25 +0530

    Add files via upload

---
+++ A.java
@@ -0,0 +1,53 @@
+public class A{//only one class can be public in a file with multiple classes
+               //classes are the basics of oops(object oriented programming language)
+public static void main(String[] args){
+System.out.println("This is class A");//system is used to return code
+        //out is a static member
+   //println is used to print text and gives output
+
+}
+}
+//public class B{=> not possible as this is to be declared into separate file B.java
+class B{//created class as B
+public static void main(String[] args){
+               //Here public is a access modifier which defines who can access this method
+		//Here static a keyword which identifies class related thing
+		//void is used to define return type of the method,void means method wont return any value
+		//main is name of method,and to display
+		//declares method String[]
+		//String[]args means arguments will be passed into main method and says that main() as a parameter
+
+System.out.println("This is class B");//system is used to return code
+        //out is a static member
+   //println is used to print text and gives output
+
+}
+}
+class C{//created class as C
+public static void main(String[] args){//Here public is a access modifier which defines who can access this method
+		//Here static a keyword which identifies class related thing
+		//void is used to define return type of the method,void means method wont return any value
+		//main is name of method,and to display
+		//declares method String[]
+		//String[]args means arguments will be passed into main method and says that main() as a parameter
+
+System.out.println("This is class C");//system is used to return code
+        //out is a static member
+   //println is used to print text and gives output
+
+}
+}
+class D{//created class as D
+public static void main(String[] args){//Here public is a access modifier which defines who can access this method
+		//Here static a keyword which identifies class related thing
+		//void is used to define return type of the method,void means method wont return any value
+		//main is name of method,and to display
+		//declares method String[]
+		//String[]args means arguments will be passed into main method and says that main() as a parameter
+
+System.out.println("This is class D");//system is used to return code
+        //out is a static member
+   //println is used to print text and gives output
+
+}
+}
